d134b26bdf4a45e0e0b1f404417866a942fc7f2f
MattAgn <matthieua@bam.tech>
2019-09-26 12:28:46 +0200

    :wrench: add movie api config file

---
+++ src/api/apiTokens.ts
@@ -0,0 +1,2 @@
+U2FsdGVkX18SSYtkqInBoIr3okazYwxw6NC0Rzd5tBBBys4pY5WAb8CyeBzBmyCm
+c3+73e81o1hl6o4vj9yBequTPwAXygkkJzscsoK8eHGG+9hjO1He+XXGJ+mzLXXu
--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,5 @@
 export const EMAIL_API_ENDPOINT =
   'https://fake-newsletter.herokuapp.com/user/subscribe'; // can be pretty slow :/
+
+export const MOVIES_API_ENDPOINT =
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1d868d05865a228a5fb2fc24c37d7b36';
