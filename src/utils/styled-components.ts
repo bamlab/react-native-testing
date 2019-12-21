aa4407066014a5d0ab6615a0b043f284bafb033d
MattAgn <matthieua@bam.tech>
2019-12-22 00:19:30 +0100

    Add lint commited

--- src/utils/styled-components.ts
+++ src/utils/styled-components.ts
@@ -2,7 +2,8 @@ import * as styledComponents from 'styled-components/native';
 import { ThemeProps as BasicThemeProps } from 'styled-components';
 import { theme } from './theme';
 
-export interface IThemeProps extends BasicThemeProps<typeof theme> {}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ThemeProps extends BasicThemeProps<typeof theme> {}
 
 const {
   default: styled,
